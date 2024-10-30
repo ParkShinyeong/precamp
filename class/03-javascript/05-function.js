2827758245ada0bae8dfe880a25d966d4b0f0ed0
ParkShinyeong <syngh503@gmail.com>
2024-10-30 16:05:36 +0900

    setInterval 중복 실행 금지 / 전화번호 입력 시 자동 포커스 이동 및 버튼 활성화
    이전에 인증번호 전송 버튼을 여러번 눌렀을 때 setInterval이 여러번 실행되어
    시간이 더 빨리 흘러가는 오류가 있었습니다.
    이를 방지하기 위해 타이머 실행 중에는 인증번호 버튼을 눌러도 기존 실행 상태에 영향을 주지 않는 코드를 작성하였습니다.
    
    DOM oninput 이벤트를 활용해 전화번호 입력 시 자동 포커스 이동 및 버튼 활성화 기능을 구현하였습니다.

--- class/03-javascript/05-function.js
+++ class/03-javascript/05-function.js
@@ -1,27 +1,58 @@
-let time = 180; 
 
+let isStarted = false; 
 document.querySelector("button").addEventListener("click", () => {
-    const randomNum = String(Math.floor(Math.random()*999999)).padStart(6, "0"); 
-    // document.getElementsByClassName("authentication__number").innerText = randomNum; 
-    /* 
-    getElementsByClassName : 단일 요소만 리턴하지 않고, 요소들의 컬렉션을 반환한다. 
-    수정하려는 요소를 지정하려면 querySelector을 사용해야 한다. 
-    */
+    if(!isStarted) {
+        // 타이머가 작동 중이 아닐 때 - 인증번호 전송 및 타이머를 시작. 
+        isStarted = true; 
+        const randomNum = String(Math.floor(Math.random()*999999)).padStart(6, "0"); 
+    
+        document.querySelector(".authentication__number").innerText = randomNum; 
+        document.querySelector(".authentication__number").style.color = "#" + randomNum
+        document.querySelector(".authentication__close").disabled = false;
 
-    document.querySelector(".authentication__number").innerText = randomNum; 
-    document.querySelector(".authentication__number").style.color = "#" + randomNum
-
-    setInterval(() => {
-        if(time >= 0) {
-            document.querySelector(".minute__number").innerText = String(Math.floor(time / 60)); 
-            document.querySelector(".second__number").innerText = String(time % 60).padStart(2, "0"); 
-            time--; 
-
-            // ? 시간이 완료되면 인증완료 버튼을 비활성화 함 
-            if(time === -1) {
+        let time = 180; 
+        let timer = setInterval(() => {
+            if(time >= 0) {
+                document.querySelector(".minute__number").innerText = String(Math.floor(time / 60)); 
+                document.querySelector(".second__number").innerText = String(time % 60).padStart(2, "0"); 
+                time--; 
+            } else {
+                // ? 시간이 완료되면 인증완료 버튼을 비활성화 함 
                 document.querySelector(".authentication__close").disabled = true; 
+                isStarted = false; 
+                console.log(isStarted)
+                clearInterval(timer); // setInterval 중단
             }
-        }
-    }, 1000)
+        }, 1000)
+    } 
 })
 
+
+// 핸드폰 번호 입력 시 자동 포커스 이동
+const phone1 = document.getElementById("phone1"); 
+const phone2 = document.getElementById("phone2");
+const phone3 = document.getElementById("phone3");  
+
+const changePhoneFocus = (tmp, next, len) => {
+    let phone  = tmp.value; 
+    if(phone.length === len ) {
+        next.focus(); 
+    }
+}
+
+phone1.addEventListener("input", () => changePhoneFocus(phone1, phone2, 3)); 
+phone2.addEventListener("input", () => changePhoneFocus(phone2, phone3, 4)); 
+
+// 전화번호가 모두 입력되었을 때 인증번호 전송 버튼을 활성화한다. 
+const disableAuthenticationButton = () => {
+    let authenticButton = document.querySelector(".authentication__button"); 
+    if (phone1.value.length === 3 && phone2.value.length === 4 && phone3.value.length === 4) {
+        authenticButton.disabled = false; 
+    } else {
+        authenticButton.disabled = true; 
+    }
+}
+
+phone1.addEventListener("input", disableAuthenticationButton)
+phone2.addEventListener("input", disableAuthenticationButton)
+phone3.addEventListener("input", disableAuthenticationButton)
